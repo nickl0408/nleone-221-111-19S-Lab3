582c8d24abb0beac59d761859fbed938f8ac7df1
Nick Leone <nicholas.leone1@marist.edu>
2019-02-14 15:20:22 -0500

    Lab 3
    Lab 3

---
+++ server.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+function onRequest(request, response){
+var q = url.parse(request.url,true);
+var filename = "." + q.pathname;
+fs.readFile(filename, function(err, data) {
+
+	if (request.url.match ('marist.jpg')){
+
+		var img = fs.readFilesSync('marist.jpg');
+		response.writeHead(200, {'Content-Type' : 'image/jpg' });
+		return response.end(img, 'binary');
+};
+	
+response.writeHead(200, {
+'Content-Type': 'text/html',
+'Access-Control-Allow-Origin' : '*'
+});
+  fs.readFile('index.html',null, function(error,data){
+		if (error) {
+			response.writeHead(404);
+			response.Write('File not found');
+		}else{
+			response.write(data);
+		}
+		response.end();
+	});
+}
+http.createServer(onRequest).listen(1337)
+
+
